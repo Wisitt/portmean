a5b8fdd756fde74ded9715e03333437de6c3383f
pufferbommy <pufferbommy@gmail.com>
2024-05-30 22:16:28 +0700

    feat: add architect page

---
+++ src/app/architect/page.tsx
@@ -0,0 +1,189 @@
+import Image from "next/image";
+
+import structureImg from "@/assets/architecture-and-interior/structure.jpg";
+import {
+  khaoyaiResort,
+  japaneseBedroom,
+  sampleImgs,
+  creativeSpacePerformanceHub,
+  adaptiveReuse,
+  ramathibodisriayuthyaMedicalCenter,
+} from "@/constants/architect";
+
+const Architect = () => {
+  return (
+    <main className="text-black py-[68px] container flex flex-col gap-[150px]">
+      <div className="h-svh flex text-5xl gap-4 justify-center font-bold items-center">
+        <div className="max-w-[400px] w-full">
+          <Image src={structureImg} alt="structure" />
+        </div>
+        <div className="flex flex-col items-center">
+          <h2 className="flex-col gap-5 flex">
+            ARCHITECTURE
+            <span className="ml-52 flex gap-5 text-primary">
+              & <span className="text-black">INTERIOR</span>
+            </span>
+          </h2>
+          <div className="grid gap-4 mt-12 grid-cols-4 grid-rows-2">
+            {sampleImgs.map((sampleImg, i) => (
+              <Image key={i} src={sampleImg} alt={`sample ${i + 1}`} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-8">
+        <h3 className="text-4xl font-bold">CREATIVE SPACE PERFORMANCE HUB</h3>
+        <div className="grid grid-rows-3 grid-cols-3 gap-4 *:h-full *:w-full">
+          <Image
+            src={creativeSpacePerformanceHub.img1}
+            className="row-span-3"
+            alt="adaptive reuse 1"
+          />
+          <Image
+            src={creativeSpacePerformanceHub.img2}
+            className="row-span-3"
+            alt="adaptive reuse 2"
+          />
+          <Image
+            src={creativeSpacePerformanceHub.img3}
+            alt="adaptive reuse 3"
+          />
+          <Image
+            src={creativeSpacePerformanceHub.img4}
+            alt="adaptive reuse 4"
+          />
+          <Image
+            src={creativeSpacePerformanceHub.img5}
+            alt="adaptive reuse 5"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-8">
+        <h3 className="text-4xl font-bold">ADAPTIVE REUSE</h3>
+        <div className="grid grid-rows-12 grid-cols-3 gap-4 *:h-full *:w-full">
+          <Image
+            src={adaptiveReuse.img1}
+            alt="adaptive reuse 1"
+            className="row-span-12"
+          />
+          <Image
+            src={adaptiveReuse.img2}
+            alt="adaptive reuse 2"
+            className="row-span-3"
+          />
+          <Image
+            src={adaptiveReuse.img6}
+            alt="adaptive reuse 6"
+            className="row-span-4"
+          />
+          <Image
+            src={adaptiveReuse.img3}
+            alt="adaptive reuse 3"
+            className="row-span-3"
+          />
+          <Image
+            src={adaptiveReuse.img7}
+            alt="adaptive reuse 7"
+            className="row-span-4"
+          />
+          <Image
+            src={adaptiveReuse.img4}
+            alt="adaptive reuse 4"
+            className="row-span-3"
+          />
+          <Image
+            src={adaptiveReuse.img8}
+            alt="adaptive reuse 8"
+            className="row-span-4"
+          />
+          <Image
+            src={adaptiveReuse.img5}
+            alt="adaptive reuse 5"
+            className="row-span-3"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-8">
+        <h3 className="text-4xl font-bold">
+          RAMATHIBODISRIAYUTHYA MEDICAL CENTER
+        </h3>
+        <div className="grid grid-rows-12 grid-cols-3 gap-4 *:w-full *:h-full">
+          <Image
+            src={ramathibodisriayuthyaMedicalCenter.img1}
+            alt="ramathibodisriayuthya medical center 1"
+            className="row-span-12"
+          />
+          <Image
+            src={ramathibodisriayuthyaMedicalCenter.img2}
+            alt="ramathibodisriayuthya medical center 2"
+            className="row-span-3"
+          />
+          <Image
+            src={ramathibodisriayuthyaMedicalCenter.img6}
+            alt="ramathibodisriayuthya medical center 6"
+            className="row-span-4"
+          />
+          <Image
+            src={ramathibodisriayuthyaMedicalCenter.img3}
+            alt="ramathibodisriayuthya medical center 3"
+            className="row-span-3"
+          />
+          <Image
+            src={ramathibodisriayuthyaMedicalCenter.img7}
+            alt="ramathibodisriayuthya medical center 7"
+            className="row-span-4"
+          />
+          <Image
+            src={ramathibodisriayuthyaMedicalCenter.img4}
+            alt="ramathibodisriayuthya medical center 4"
+            className="row-span-3"
+          />
+          <Image
+            src={ramathibodisriayuthyaMedicalCenter.img8}
+            alt="ramathibodisriayuthya medical center 8"
+            className="row-span-4"
+          />
+          <Image
+            src={ramathibodisriayuthyaMedicalCenter.img5}
+            alt="ramathibodisriayuthya medical center 5"
+            className="row-span-3"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-8">
+        <h3 className="text-4xl font-bold">KHAOYAI RESORT</h3>
+        <div className="grid grid-cols-3 grid-rows-3 gap-4 *:w-full *:h-full">
+          <Image src={khaoyaiResort.img1} alt="khaoyai resort 1" />
+          <Image src={khaoyaiResort.img4} alt="khaoyai resort 4" />
+          <Image
+            src={khaoyaiResort.img7}
+            alt="khaoyai resort 7"
+            className="row-span-3"
+          />
+          <Image src={khaoyaiResort.img2} alt="khaoyai resort 2" />
+          <Image src={khaoyaiResort.img5} alt="khaoyai resort 5" />
+          <Image src={khaoyaiResort.img3} alt="khaoyai resort 3" />
+          <Image src={khaoyaiResort.img6} alt="khaoyai resort 6" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-8">
+        <h3 className="text-4xl font-bold">JAPANESE BEDROOM</h3>
+        <div className="grid grid-cols-3 grid-rows-3 gap-4 *:w-full *:h-full">
+          <Image src={japaneseBedroom.img1} alt="japanese bedroom 1" />
+          <Image src={japaneseBedroom.img2} alt="japanese bedroom 2" />
+          <Image
+            src={japaneseBedroom.img3}
+            alt="japanese bedroom 3"
+            className="row-span-3"
+          />
+          <Image src={japaneseBedroom.img4} alt="japanese bedroom 4" />
+          <Image src={japaneseBedroom.img5} alt="japanese bedroom 5" />
+          <Image src={japaneseBedroom.img6} alt="khaoyai resort 6" />
+          <Image src={japaneseBedroom.img7} alt="japanese bedroom 7" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Architect;
---
+++ src/constants/architect.ts
@@ -0,0 +1,105 @@
+import sample1Img from "@/assets/architecture-and-interior/sample-1.png";
+import sample2Img from "@/assets/architecture-and-interior/sample-2.png";
+import sample3Img from "@/assets/architecture-and-interior/sample-3.png";
+import sample4Img from "@/assets/architecture-and-interior/sample-4.png";
+import sample5Img from "@/assets/architecture-and-interior/sample-5.png";
+import sample6Img from "@/assets/architecture-and-interior/sample-6.png";
+
+import khaoyaiResort1Img from "@/assets/architecture-and-interior/khaoyai-resort/1.jpg";
+import khaoyaiResort2Img from "@/assets/architecture-and-interior/khaoyai-resort/2.jpg";
+import khaoyaiResort3Img from "@/assets/architecture-and-interior/khaoyai-resort/3.jpg";
+import khaoyaiResort4Img from "@/assets/architecture-and-interior/khaoyai-resort/4.jpg";
+import khaoyaiResort5Img from "@/assets/architecture-and-interior/khaoyai-resort/5.jpg";
+import khaoyaiResort6Img from "@/assets/architecture-and-interior/khaoyai-resort/6.jpg";
+import khaoyaiResort7Img from "@/assets/architecture-and-interior/khaoyai-resort/7.jpg";
+
+import creativeSpacePerformanceHub1Img from "@/assets/architecture-and-interior/creative-space-performance-hub/1.jpg";
+import creativeSpacePerformanceHub2Img from "@/assets/architecture-and-interior/creative-space-performance-hub/2.png";
+import creativeSpacePerformanceHub3Img from "@/assets/architecture-and-interior/creative-space-performance-hub/3.jpg";
+import creativeSpacePerformanceHub4Img from "@/assets/architecture-and-interior/creative-space-performance-hub/4.jpg";
+import creativeSpacePerformanceHub5Img from "@/assets/architecture-and-interior/creative-space-performance-hub/5.jpg";
+
+import japaneseBedroom1Img from "@/assets/architecture-and-interior/japanese-bedroom/1.jpg";
+import japaneseBedroom2Img from "@/assets/architecture-and-interior/japanese-bedroom/2.jpg";
+import japaneseBedroom3Img from "@/assets/architecture-and-interior/japanese-bedroom/3.jpg";
+import japaneseBedroom4Img from "@/assets/architecture-and-interior/japanese-bedroom/4.jpg";
+import japaneseBedroom5Img from "@/assets/architecture-and-interior/japanese-bedroom/5.jpg";
+import japaneseBedroom6Img from "@/assets/architecture-and-interior/japanese-bedroom/6.jpg";
+import japaneseBedroom7Img from "@/assets/architecture-and-interior/japanese-bedroom/7.jpg";
+
+import adaptiveReuse1Img from "@/assets/architecture-and-interior/adaptive-reuse/1.jpg";
+import adaptiveReuse2Img from "@/assets/architecture-and-interior/adaptive-reuse/2.jpg";
+import adaptiveReuse3Img from "@/assets/architecture-and-interior/adaptive-reuse/3.jpg";
+import adaptiveReuse4Img from "@/assets/architecture-and-interior/adaptive-reuse/4.jpg";
+import adaptiveReuse5Img from "@/assets/architecture-and-interior/adaptive-reuse/5.jpg";
+import adaptiveReuse6Img from "@/assets/architecture-and-interior/adaptive-reuse/6.jpg";
+import adaptiveReuse7Img from "@/assets/architecture-and-interior/adaptive-reuse/7.jpg";
+import adaptiveReuse8Img from "@/assets/architecture-and-interior/adaptive-reuse/8.jpg";
+
+import ramathibodisriayuthyaMedicalCenter1Img from "@/assets/architecture-and-interior/ramathibodisriayuthya-medical-center/1.jpg";
+import ramathibodisriayuthyaMedicalCenter2Img from "@/assets/architecture-and-interior/ramathibodisriayuthya-medical-center/2.jpg";
+import ramathibodisriayuthyaMedicalCenter3Img from "@/assets/architecture-and-interior/ramathibodisriayuthya-medical-center/3.jpg";
+import ramathibodisriayuthyaMedicalCenter4Img from "@/assets/architecture-and-interior/ramathibodisriayuthya-medical-center/4.jpg";
+import ramathibodisriayuthyaMedicalCenter5Img from "@/assets/architecture-and-interior/ramathibodisriayuthya-medical-center/5.jpg";
+import ramathibodisriayuthyaMedicalCenter6Img from "@/assets/architecture-and-interior/ramathibodisriayuthya-medical-center/6.jpg";
+import ramathibodisriayuthyaMedicalCenter7Img from "@/assets/architecture-and-interior/ramathibodisriayuthya-medical-center/7.jpg";
+import ramathibodisriayuthyaMedicalCenter8Img from "@/assets/architecture-and-interior/ramathibodisriayuthya-medical-center/8.jpg";
+
+export const sampleImgs = [
+  sample1Img,
+  sample2Img,
+  sample3Img,
+  sample4Img,
+  sample5Img,
+  sample6Img,
+];
+
+export const adaptiveReuse = {
+  img1: adaptiveReuse1Img,
+  img2: adaptiveReuse2Img,
+  img3: adaptiveReuse3Img,
+  img4: adaptiveReuse4Img,
+  img5: adaptiveReuse5Img,
+  img6: adaptiveReuse6Img,
+  img7: adaptiveReuse7Img,
+  img8: adaptiveReuse8Img,
+};
+
+export const creativeSpacePerformanceHub = {
+  img1: creativeSpacePerformanceHub1Img,
+  img2: creativeSpacePerformanceHub2Img,
+  img3: creativeSpacePerformanceHub3Img,
+  img4: creativeSpacePerformanceHub4Img,
+  img5: creativeSpacePerformanceHub5Img,
+};
+
+export const khaoyaiResort = {
+  img1: khaoyaiResort1Img,
+  img2: khaoyaiResort2Img,
+  img3: khaoyaiResort3Img,
+  img4: khaoyaiResort4Img,
+  img5: khaoyaiResort5Img,
+  img6: khaoyaiResort6Img,
+  img7: khaoyaiResort7Img,
+};
+
+export const japaneseBedroom = {
+  img1: japaneseBedroom1Img,
+  img2: japaneseBedroom2Img,
+  img3: japaneseBedroom3Img,
+  img4: japaneseBedroom4Img,
+  img5: japaneseBedroom5Img,
+  img6: japaneseBedroom6Img,
+  img7: japaneseBedroom7Img,
+};
+
+export const ramathibodisriayuthyaMedicalCenter = {
+  img1: ramathibodisriayuthyaMedicalCenter1Img,
+  img2: ramathibodisriayuthyaMedicalCenter2Img,
+  img3: ramathibodisriayuthyaMedicalCenter3Img,
+  img4: ramathibodisriayuthyaMedicalCenter4Img,
+  img5: ramathibodisriayuthyaMedicalCenter5Img,
+  img6: ramathibodisriayuthyaMedicalCenter6Img,
+  img7: ramathibodisriayuthyaMedicalCenter7Img,
+  img8: ramathibodisriayuthyaMedicalCenter8Img,
+};
