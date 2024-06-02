a6ce32bab82d79cb0341491012cc72d081d178a1
pufferbommy <pufferbommy@gmail.com>
2024-06-02 17:19:27 +0700

    feat: responsive uxui

--- src/app/uxui/page.tsx
+++ src/app/uxui/page.tsx
@@ -9,20 +9,20 @@ import illustration2Img from "@/assets/illustration-2.png";
 
 const Uxui = () => {
   return (
-    <div className="pt-[68px] overflow-hidden h-screen container place-content-center text-center grid md:grid-cols-2 gap-4 text-black text-3xl font-bold">
+    <div className="pt-[68px] min-h-screen container place-content-center text-center grid md:grid-cols-2 gap-4 text-black text-3xl font-bold">
       <Link
         href="/uxui/lingual-lougne"
         className="group p-12 flex flex-col gap-[25px] items-center relative"
       >
-        <div className="h-8 lg:h-9 overflow-hidden">
-          <div className="group-hover:-translate-y-8 lg:group-hover:-translate-y-9 transition-transform duration-200">
+        <div className="lg:h-9 lg:overflow-hidden">
+          <div className="lg:group-hover:-translate-y-9 transition-transform duration-200">
             <h2>WEBSITE</h2>
             <h2 className="text-[#004743]">LINGUAL LOUGNE</h2>
           </div>
         </div>
         <div className="relative">
-          <Image className="" src={iphoneImg} alt="iphone" />
-          <div className="group-hover:opacity-100 duration-200 opacity-0 absolute top-0 left-[calc(100%+39px)]">
+          <Image src={iphoneImg} alt="iphone" />
+          <div className="opacity-100 lg:opacity-0 group-hover:opacity-100 duration-200 absolute top-0 left-[calc(100%+39px)]">
             <ColorPalette
               colors={[
                 "#001E1E",
@@ -37,24 +37,28 @@ const Uxui = () => {
           </div>
         </div>
         <Image
-          className="group-hover:opacity-100 duration-200 opacity-0 absolute left-[1.7%] -bottom-[13%]"
+          className="hidden lg:block opacity-0 group-hover:opacity-100 duration-200 absolute left-[1.7%] -bottom-[13%]"
           src={illustration1Img}
           alt="illustration 1"
         />
       </Link>
       <Link
         href="/uxui/keef"
-        className="group p-12 flex flex-col gap-[25px] items-center cursor-pointer relative"
+        className="group p-12 flex flex-col gap-[25px] items-center relative"
       >
-        <div className="h-8 lg:h-9 overflow-hidden">
-          <div className="group-hover:-translate-y-8 lg:group-hover:-translate-y-9 transition-transform duration-200">
+        <div className="lg:h-9 lg:overflow-hidden">
+          <div className="lg:group-hover:-translate-y-9 transition-transform duration-200">
             <h2>MOBILE APPLICATION</h2>
             <h2 className="text-[#4C5C6C]">KEEF</h2>
           </div>
         </div>
-        <div className="relative">
-          <Image src={macbookImg} alt="macbook" />
-          <div className="group-hover:opacity-100 duration-200 opacity-0 absolute top-[calc(100%+25px)] left-0">
+        <div className="relative h-full">
+          <Image
+            className="w-full h-full object-contain"
+            src={macbookImg}
+            alt="macbook"
+          />
+          <div className="opacity-100 lg:opacity-0 group-hover:opacity-100 duration-200 absolute top-[calc(100%+25px)] left-0">
             <ColorPalette
               colors={[
                 "#272622",
@@ -68,7 +72,7 @@ const Uxui = () => {
           </div>
         </div>
         <Image
-          className="group-hover:opacity-100 duration-200 opacity-0 absolute -right-[9%] -bottom-[14%]"
+          className="lg:group-hover:opacity-100 duration-200 opacity-0 absolute -right-[9%] -bottom-[14%]"
           src={illustration2Img}
           alt="illustration 2"
         />
